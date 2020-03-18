0bbf138c8e9816db2788e225de2c1d2a80428b75
Loïs BICORNE <45924522+Jikyurs@users.noreply.github.com>
2020-03-18 15:05:47 +0100

    Update ex2.php

--- td1/tp1/ex2.php
+++ td1/tp1/ex2.php
@@ -1,8 +1,12 @@
 <?php
-$size = $_GET["size"];
-$color = $_GET["color"];
-$message = $_GET["message"];
-echo "<div style='font-size: {$size}px;color: {$color};'>{$message}</div>";
+  $size = $_GET["size"];
+  $color = $_GET["color"];
+  $message = $_GET["message"];
+
+  if (empty($_GET)) {
+    echo "Aucun paramètres !";
+  }
+  echo "<div style='font-size: {$size}px;color: {$color};'>{$message}</div>";
 ?>
 <a href="/web-dyna-td1/td1/tp1/ex2.php?message=Hello There&size=15&color=red">Message rouge de 15px</a>
 <br>
