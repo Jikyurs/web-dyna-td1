93f3ba37fe3873911e248a56af8b549ee1f3a3a5
Loïs BICORNE <45924522+Jikyurs@users.noreply.github.com>
2020-03-18 14:37:48 +0100

    Update ex2.php

---
+++ td1/tp1/ex2.php
@@ -0,0 +1,11 @@
+<?php
+$size = $_GET["size"];
+$color = $_GET["color"];
+$message = $_GET["message"];
+echo "<div style='font-size: {$size}px;color: {$color};'>{$message}</div>";
+?>
+<a href="/web-dyna-td1/td1/tp1/ex2.php?message=Hello There&size=15&color=red">Message rouge de 15px</a>
+<br>
+<a href="/web-dyna-td1/td1/tp1/ex2.php?message=General Kenobi&size=30&color=green">Message vert de 30px</a>
+<br>
+<a href="/web-dyna-td1/td1/tp1/ex2.php?message=Ah yes, the negociator&size=50&color=blue">Message bleu de 50px</a>
