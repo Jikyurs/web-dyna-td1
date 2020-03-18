830c8ec441fd2197dbc428692cc9eff482aa468a
Loïs BICORNE <45924522+Jikyurs@users.noreply.github.com>
2020-03-18 14:07:14 +0100

    premier commit : ex1 et 2

---
+++ td1/tp1/ex1.php
@@ -0,0 +1,9 @@
+<?php
+echo "Message : ".$_GET["message"];
+?>
+<br>
+<a href="/web-dyna-td1/td1/tp1/ex1.php?message=Hello">Afficher Hello</a>
+<br>
+<a href="/web-dyna-td1/td1/tp1/ex1.php?message=Kenobi">Afficher Kenobi</a>
+<br>
+<a href="/web-dyna-td1/td1/tp1/ex1.php?message=Negociator">Afficher Negociator</a>
