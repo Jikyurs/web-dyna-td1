92f5fa3e8d7fc0075dcfc16a7ac3939dd705e815
Loïs BICORNE <45924522+Jikyurs@users.noreply.github.com>
2020-03-19 11:42:16 +0100

    Update ex2.php

--- td1/tp1/ex2.php
+++ td1/tp1/ex2.php
@@ -4,7 +4,11 @@
   $message = $_GET["message"];
 
   if (empty($_GET)) {
-    echo "Aucun paramètres !";
+    echo "ATTENTION ! Aucun paramètres renseignés !";
+  }
+  if (empty($color) and empty($size)) {
+    $size = 12;
+    $color = "black";
   }
   echo "<div style='font-size: {$size}px;color: {$color};'>{$message}</div>";
 ?>
@@ -13,3 +17,13 @@
 <a href="/web-dyna-td1/td1/tp1/ex2.php?message=General Kenobi&size=30&color=green">Message vert de 30px</a>
 <br>
 <a href="/web-dyna-td1/td1/tp1/ex2.php?message=Ah yes, the negociator&size=50&color=blue">Message bleu de 50px</a>
+<br>
+<form method="GET">
+    <label for="$message">Message : </label>
+    <input type="text" value="" name="message" id="message">
+    <label for="$size">Size : </label>
+    <input type="number" value="" name="size" id="size">
+    <label for="$color">Color : </label>
+    <input type="color" value="" name="color" id="color">
+    <input type="submit" value="Valider">
+</form>
