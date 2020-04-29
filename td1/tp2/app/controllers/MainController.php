314317519b3b316d7a99117c4acd0822572ff936
Loïs BICORNE <45924522+Jikyurs@users.noreply.github.com>
2020-04-29 16:40:55 +0200

    TP2-3.1

--- td1/tp2/app/controllers/MainController.php
+++ td1/tp2/app/controllers/MainController.php
@@ -11,7 +11,13 @@ class MainController extends AbstractWsController{
      *
      * @get("_default","name"=>"Home")
      */
-    public function index() {}
+    public function index() {
+        $menu=$this->getMenu('Home');
+        $messages=$this->dataProvider->getMessages();
+        $content=nl2br($this->dataProvider->getPageContent('Home'));
+        $this->loadView('MainController/index.html',compact('messages','content') + $menu);
+    }
+
 
 	/**
 	 *@route("contact","methods"=>["get"],"name"=>"Contact")
